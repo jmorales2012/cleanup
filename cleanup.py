9afbfb766c99e9f7db4c5b98d77f48bb61b8a749
Joshua Morales <Joshua@Joshuas-MacBook-Air.local>
2015-09-08 11:41:24 -0600

    Added option to exclude specific filetypes with -e arg

--- cleanup.py
+++ cleanup.py
@@ -6,32 +6,39 @@ import send2trash
 # set args to allow for permanent deletion and to specify file ext to delete
 parser = argparse.ArgumentParser(description='Delete files in a folder with a \
                                  given file extension')
-parser.add_argument('extension', help='Specify file extensions to delete \
+parser.add_argument('delete', help='Specify file extensions to delete \
                     separated by a space. \'all\' to delete everything.',
                     type=str)
 parser.add_argument('-p', '--permanent', help='Permanently delete the files',
                     action='store_true', default=False)
+parser.add_argument('-e', '--exclude', help='Exclude the following files.',
+                    type=str, default='')
 args = parser.parse_args()
 
-# create list to delete multiple extensions at once
-extensions = tuple(args.extension.split())
+# create tuples with files to delete and files to exclude
+delete = tuple(args.delete.split())
+exclude = tuple(args.exclude.split())
 
-# loop through each file in pwd and delete if specified in args
-for filename in os.listdir():
-    # deletes all files if specified
-    if args.extension == 'all':
-        if args.permanent:
-            print('Permanently Deleted: ' + filename)
-            os.unlink(filename)
-        else:
-            print('Sent to trash: ' + filename)
-            send2trash.send2trash(filename)
 
-    # deletes files ending with specified extensions
-    elif filename.endswith(extensions):
-        if args.permanent:
-            print('Permanently Deleted: ' + filename)
-            os.unlink(filename)
-        else:
-            print('Sent to trash: ' + filename)
-            send2trash.send2trash(filename)
+def deleteFiles(deleteFiles, excludeFiles):
+    # loop through each file in pwd and delete if specified in args
+    for filename in os.listdir():
+        # deletes all files if specified unless that file is excluded
+        if 'all' in deleteFiles and not filename.endswith(excludeFiles):
+            if args.permanent:
+                print('Permanently Deleted: ' + filename)
+                os.unlink(filename)
+            else:
+                print('Sent to trash: ' + filename)
+                send2trash.send2trash(filename)
+
+        # deletes files ending with specified extensions
+        elif filename.endswith(deleteFiles):
+            if args.permanent:
+                print('Permanently Deleted: ' + filename)
+                os.unlink(filename)
+            else:
+                print('Sent to trash: ' + filename)
+                send2trash.send2trash(filename)
+
+deleteFiles(delete, exclude)
